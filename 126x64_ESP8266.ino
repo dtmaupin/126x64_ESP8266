15a204ac65b7810e09401f9953bb0fbfffef94e5
Daniel Maupin <dmaupin@live.com>
2018-05-22 23:30:14 -0400

    With WIFI

--- 126x64_ESP8266.ino
+++ 126x64_ESP8266.ino
@@ -1,37 +1,148 @@
-/* How to use the DHT-22 sensor with Arduino uno
-   Temperature and humidity sensor
-*/
-
-//Libraries
-#include <DHT.h>;
-
-//Constants
-#define DHTPIN 10     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-//Constants sensor 2
-#define DHT2PIN 9     // what pin we're connected to
-DHT dht2(DHT2PIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-
-//Variables
-int chk;
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
-float tempF; //Stores Temp in F value
-float hum2; //Stores humidity 2 value
-float temp2; //Stores temp2 value
-float temp2F; //Stores temp2 F value
+/**
+ * Example for sending temperature and humidity
+ * to the cloud using the DHT22 and ESP8266
+ *
+ * Copyright (c) 2016 Losant IoT. All rights reserved.
+ * https://www.losant.com
+ */
 
 #include <Wire.h>
 #include <LiquidCrystal_PCF8574.h>
 #include "DHT.h"
-int timeSinceLastRead = "0";
-LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Losant.h>
 
+#define DHTPIN 10     // what digital pin the DHT22 is conected to
+#define DHTTYPE DHT22   // There are multiple kinds of DHT sensors
+
+
+LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 int show;
 
-void setup()
-{
+DHT dht(DHTPIN, DHTTYPE);
+
+// WiFi credentials.
+const char* WIFI_SSID = "TheDink";
+const char* WIFI_PASS = "Odmdink1023";
+
+// Losant credentials.
+const char* LOSANT_DEVICE_ID = "5b04df97d17e6e0007daeee4";
+const char* LOSANT_ACCESS_KEY = "373711f0-c580-441c-9141-1637032058de";
+const char* LOSANT_ACCESS_SECRET = "4212df0490eaea6a72c2dfa5972c4a816ac8635634da6b7096c390ed0930fe98";
+
+
+WiFiClientSecure wifiClient;
+
+LosantDevice device(LOSANT_DEVICE_ID);
+
+void connect() {
+
+  // Connect to Wifi.
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  unsigned long wifiConnectStart = millis();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    // Check to see if
+    if (WiFi.status() == WL_CONNECT_FAILED) {
+      Serial.println("Failed to connect to WIFI. Please verify credentials: ");
+      Serial.println();
+      Serial.print("SSID: ");
+      Serial.println(WIFI_SSID);
+      Serial.print("Password: ");
+      Serial.println(WIFI_PASS);
+      Serial.println();
+    }
+
+    delay(500);
+    Serial.println("...");
+    // Only try for 5 seconds.
+    if(millis() - wifiConnectStart > 5000) {
+      Serial.println("Failed to connect to WiFi");
+      Serial.println("Please attempt to send updated configuration parameters.");
+      return;
+    }
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Serial.print("Authenticating Device...");
+  HTTPClient http;
+  http.begin("http://api.losant.com/auth/device");
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Accept", "application/json");
+
+  /* Create JSON payload to sent to Losant
+   *
+   *   {
+   *     "deviceId": "575ecf887ae143cd83dc4aa2",
+   *     "key": "this_would_be_the_key",
+   *     "secret": "this_would_be_the_secret"
+   *   }
+   *
+   */
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["deviceId"] = LOSANT_DEVICE_ID;
+  root["key"] = LOSANT_ACCESS_KEY;
+  root["secret"] = LOSANT_ACCESS_SECRET;
+  String buffer;
+  root.printTo(buffer);
+
+  int httpCode = http.POST(buffer);
+
+  if(httpCode > 0) {
+      if(httpCode == HTTP_CODE_OK) {
+          Serial.println("This device is authorized!");
+      } else {
+        Serial.println("Failed to authorize device to Losant.");
+        if(httpCode == 400) {
+          Serial.println("Validation error: The device ID, access key, or access secret is not in the proper format.");
+        } else if(httpCode == 401) {
+          Serial.println("Invalid credentials to Losant: Please double-check the device ID, access key, and access secret.");
+        } else {
+           Serial.println("Unknown response from API");
+        }
+      }
+    } else {
+        Serial.println("Failed to connect to Losant API.");
+
+   }
+
+  http.end();
+
+  // Connect to Losant.
+  Serial.println();
+  Serial.print("Connecting to Losant...");
+
+  device.connectSecure(wifiClient, LOSANT_ACCESS_KEY, LOSANT_ACCESS_SECRET);
+
+  while(!device.connected()) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("Connected!");
+  Serial.println();
+  Serial.println("This device is now ready for use!");
+}
+
+void setup() {
   int error;
 
   Serial.begin(115200);
@@ -58,89 +169,115 @@ void setup()
 
   lcd.begin(20, 4); // initialize the lcd
   show = 0;
-} // setup()
+  
+    Serial.println("Device Started");
+  Serial.println("-------------------------------------");
+  Serial.println("Running DHT!");
+  Serial.println("-------------------------------------");
 
-void loop()
-{
-  if (show == 0) {
-    if (timeSinceLastRead == 0 || timeSinceLastRead > 5000) {
-    //Read data and store it to variables hum and temp
-    hum = dht.readHumidity();
-    temp= dht.readTemperature();
-    tempF= dht.readTemperature(true);
-    hum2 = dht2.readHumidity();
-    temp2 = dht2.readTemperature();
-    temp2F = dht2.readTemperature(true);
+  connect();
+}
+
+void report(double humidity, double tempC, double tempF, double heatIndexC, double heatIndexF) {
+  StaticJsonBuffer<500> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["humidity"] = humidity;
+  root["tempC"] = tempC;
+  root["tempF"] = tempF;
+  root["heatIndexC"] = heatIndexC;
+  root["heatIndexF"] = heatIndexF;
+  device.sendState(root);
+  Serial.println("Reported!");
+}
+
+int timeSinceLastRead = 0;
+void loop() {
+   bool toReconnect = false;
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Disconnected from WiFi");
+    toReconnect = true;
+  }
+
+  if (!device.connected()) {
+    Serial.println("Disconnected from MQTT");
+    Serial.println(device.mqttClient.state());
+    toReconnect = true;
+  }
+
+  if (toReconnect) {
+    connect();
+  }
+
+  device.loop();
+
+  // Report every 2 seconds.
+  if(timeSinceLastRead > 15000) {
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f)) {
+      Serial.println("Failed to read from DHT sensor!");
+      timeSinceLastRead = 0;
+      return;
+    }
+
+    // Compute heat index in Fahrenheit (the default)
+    float hif = dht.computeHeatIndex(f, h);
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t");
+    Serial.print("Heat index: ");
+    Serial.print(hic);
+    Serial.print(" *C ");
+    Serial.print(hif);
+    Serial.println(" *F");
+
+    // LCD Section 
     lcd.noCursor();
     lcd.setBacklight(255);
     lcd.home(); lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Humidity: ");
-    lcd.print(hum);
+    lcd.print(h);
     lcd.print("%");
     lcd.setCursor(0, 1);
     lcd.print("Temp C: ");
-    lcd.print(temp);
+    lcd.print(t);
     lcd.print("'");
     lcd.setCursor(0, 2);
     lcd.print("Temp F: ");
-    lcd.print(tempF);
+    lcd.print(f);
     lcd.print("'");
     lcd.setCursor(0, 3);
     // Compute heat index in Fahrenheit (the default)
-    float hif = dht.computeHeatIndex(tempF, hum);
-    float hif2 = dht.computeHeatIndex(temp2F, hum);
+    float LCDhif = dht.computeHeatIndex(f, h);
     lcd.print("Heat Index F: ");
-    lcd.print(hif);
+    lcd.print(LCDhif);
     lcd.print("'");
- // End of sensor 1 output
-    delay(5000); //Delay 1 second
-//  Start of sensor 2 output
-    lcd.home(); lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Humidity #2: ");
-    lcd.print(hum2);
-    lcd.print("%");
-    lcd.setCursor(0, 1);
-    lcd.print("Temp #2 C: ");
-    lcd.print(temp2);
-    lcd.print("'");
-    lcd.setCursor(0, 2);
-    lcd.print("Temp #2 F: ");
-    lcd.print(temp2F);
-    lcd.print("'");
-    lcd.setCursor(0, 3);
-    lcd.print("Heat Index F: ");
-    lcd.print(hif2);
-    lcd.print("'");
-    //End of Sensor 2 Section
     
-    //Print temp and humidity values to serial monitor
-    Serial.print("Humidity: ");
-    Serial.print(hum);
-    Serial.print(" %, Temp: ");
-    Serial.print(temp);
-    Serial.print(" Celsius, Temp: ");
-    Serial.print(tempF);
-    Serial.print(" Deg F, Heat Index:");
-    Serial.print(hif);
-    Serial.println(" Def F");
-    Serial.print("Humidity2: ");
-    Serial.print(hum2);
-    Serial.print(" %, Temp2: ");
-    Serial.print(temp2);
-    Serial.print(" Celsius, Temp2: ");
-    Serial.print(temp2F);
-    Serial.print(" Deg F, Heat Index2:");
-    Serial.print(hif2);
-    Serial.println(" Def F");
-    //lcd.setBacklight(255);
-    timeSinceLastRead = 1;
-    }
-    else {
-      delay(100); //delay 100 ms
-      timeSinceLastRead += 100;
-    }
-    }
+    report(h, t, f, hic, hif);
+
+    timeSinceLastRead = 0;
   }
+  delay(100);
+  timeSinceLastRead += 100;
+}
+
+
+
 
